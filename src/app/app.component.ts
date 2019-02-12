660f8147111823064e2e7e4556474620a6b23f29
Mohamed El-Ghoneimy <ghoneishy@hotmail.com>
2019-02-12 13:50:38 +0200

    resolve merge conflict

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -17,23 +16,3 @@ export class AppComponent {
     translate.use('en');
   }
 }
-=======
-import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
-export class AppComponent {
-  title = 'NewSmartHolding';
-  constructor(public translate: TranslateService) {
-    // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('en');
-
-    // the lang to use, if the lang isn't available, it will use the current loader to get them
-    translate.use('en');
-  }
-}
->>>>>>> c8e6090ea0df198445614dcae89f7e1034d1ee7f
